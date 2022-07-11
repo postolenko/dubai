e0ff9c07ef172090f58adfb649b9ebae7ee23a27
postolenko <denis.postolenko@gmail.com>
2022-07-11 22:04:13 +0300

    D

---
+++ README.md
@@ -0,0 +1,1 @@
+# http://postolenko.github.io/dubai/index.html
---
+++ js/scripts.js
@@ -0,0 +1,115 @@
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+
+$(window).load(function() {
+
+
+
+});
+
+$(window).resize(function() {
+
+
+
+});
+
+$(document).scroll(function() {
+
+
+
+});
+
+$(document).ready(function() {
+
+    if( $(".slider").length > 0 ) {
+      $(".slider").not(".slick-initialized").slick({
+          dots: false,
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 4000,
+          speed: 1200,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          // fade: true,
+          // responsive: [
+          //     {
+          //       breakpoint: 900,
+          //       settings: {
+          //         slidesToShow: 2,
+          //         slidesToScroll: 2
+          //       }
+          //     },
+          //     {
+          //       breakpoint: 540,
+          //       settings: {
+          //         slidesToShow: 1,
+          //         slidesToScroll: 1
+          //       }
+          //     }
+          //   ]
+      });
+    }
+
+    if( $(".testimonials_slider").length > 0 ) {
+      $(".testimonials_slider").not(".slick-initialized").slick({
+          dots: false,
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 4000,
+          speed: 1200,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          // fade: true,
+          // responsive: [
+          //     {
+          //       breakpoint: 900,
+          //       settings: {
+          //         slidesToShow: 2,
+          //         slidesToScroll: 2
+          //       }
+          //     },
+          //     {
+          //       breakpoint: 540,
+          //       settings: {
+          //         slidesToShow: 1,
+          //         slidesToScroll: 1
+          //       }
+          //     }
+          //   ]
+      });
+    }
+
+    if( $(".portfolio_slider").length > 0 ) {
+        $(".portfolio_slider").not(".slick-initialized").slick({
+            dots: true,
+            arrows: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 1200,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            // fade: true,
+            responsive: [
+                {
+                  breakpoint: 900,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                },
+                {
+                  breakpoint: 540,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+              ]
+        });
+    }
+
+});
