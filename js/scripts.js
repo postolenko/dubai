75704acfe2dd02b272d51a918b515c8faa04b25b
postolenko <denis.postolenko@gmail.com>
2022-07-12 04:23:31 +0300

    D

--- js/scripts.js
+++ js/scripts.js
@@ -83,32 +83,49 @@ $(document).ready(function() {
       });
     }
 
-    if( $(".portfolio_slider").length > 0 ) {
-        $(".portfolio_slider").not(".slick-initialized").slick({
-            dots: true,
+    // ----------------
+
+    $(".tab_link").click(function(e) {
+      parent = $(this).closest(".tab_list");
+      parent.find(".tab_link").removeClass("active");
+      $(this).addClass("active");
+    });
+
+    // ----------------
+
+    $(".tab_link_2").click(function(e) {
+      if(!$(this).hasClass("active")) {
+        parent = $(this).closest(".tab_list_2");
+        parent.find(".tab_link_2").removeClass("active");
+        $(this).addClass("active");
+        $(".slider_2").not(".slick-initialized").slick({
+            dots: false,
+            arrows: true,
+            // autoplay: true,
+            // autoplaySpeed: 4000,
+            speed: 1200,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><img src="img/slick_prev.svg"></button>',
+            nextArrow: '<button class="slick-next" aria-label="Next" type="button"><img src="img/slick_next.svg"></button>'
+        });
+      }
+      
+    });
+
+    if( $(".slider_2").length > 0 ) {
+        $(".slider_2").not(".slick-initialized").slick({
+            dots: false,
             arrows: true,
             autoplay: true,
             autoplaySpeed: 4000,
             speed: 1200,
-            slidesToShow: 4,
+            slidesToShow: 1,
             slidesToScroll: 1,
-            // fade: true,
-            responsive: [
-                {
-                  breakpoint: 900,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                  }
-                },
-                {
-                  breakpoint: 540,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }
-              ]
+            fade: true,
+            prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><img src="img/slick_prev.svg"></button>',
+            nextArrow: '<button class="slick-next" aria-label="Next" type="button"><img src="img/slick_next.svg"></button>'
         });
     }
 
